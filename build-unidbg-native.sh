95cdf0e71ee93497bb3fde954b9df4067e53a0cb
Hello666cpu <301962298+Hello666cpu@users.noreply.github.com>
2026-08-18 21:31:37 +0800

    fix(license): add GPL-3.0-only header to build-unidbg-native.sh

--- build-unidbg-native.sh
+++ build-unidbg-native.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# SOMCP - build-unidbg-native.sh
+# Copyright (C) 2026 SOMCP authors
+# Upstream: https://github.com/bilieebiliee1-design/SOMCP
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>.
+#
 # build-unidbg-native.sh - Cross-compile the Android native libraries required
 # by the Unidbg emulation backend using the Android NDK.
 #
